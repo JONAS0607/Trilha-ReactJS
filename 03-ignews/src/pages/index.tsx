02f2b5f87de3fef41715b2ed1226fcebf1514e04
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-11-10 22:04:08 -0300

    configurado sass, typescript e global.scss

---
+++ 03-ignews/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.News</title>
+      </Head>
+      <h1>Olá Novamente!</h1>
+    </>
+  );
+}
