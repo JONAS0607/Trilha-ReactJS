f51a0f8f38dd3ab732adff044d8ca3d046f5b329
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-11-11 12:23:20 -0300

    finalizado módulo 3

--- 03-ignews/src/components/SignInButton/index.tsx
+++ 03-ignews/src/components/SignInButton/index.tsx
@@ -2,13 +2,13 @@ import { FaGithub } from "react-icons/fa";
 import styles from "./styles.module.scss";
 import { FiX } from "react-icons/fi";
 export function SingInButton() {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
 
   return isUserLoggedIn ? (
     <button type="button" className={styles.SignInButton}>
       <FaGithub color="#04d361" />
       Jonas Cabral
-      <FiX color="#737380"  className={styles.closeIcon}/>
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button type="button" className={styles.SignInButton}>
--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps } from "next";
+// import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -10,6 +11,17 @@ interface HomeProps {
     amount: number;
   };
 }
+/**
+ * Formas de chamado no next para API
+ * -Client-side --> (CSR) BROWSER 
+ * -Server-side (SSR) -> BUSCAR DADOS DO PRODUTO/USUÁRIO
+ * -Static (SSG) --> PÁGINAS ESTÁTICAS
+ * 
+ * EXEMPLOS DE USO
+ * -Conteúdo -> (SSG)
+ * -Comentários -> tempo real-> (Client-side)
+ * -
+ */
 
 export default function Home({ product }: HomeProps) {
   return (
@@ -35,7 +47,8 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+// export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1JudczKwICLeQV4RJHPj5Y1m");
   const product = {
     priceId: price.id,
@@ -48,5 +61,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24hr
   };
 };
