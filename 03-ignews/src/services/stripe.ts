0bb3308dc1b8e3d47d929fa2468d270de70f3df1
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-11-12 19:30:06 -0300

    stripe e fauna ok, dados guardados e CLI do stripe ok subscription ok

--- 03-ignews/src/services/stripe.ts
+++ 03-ignews/src/services/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
-    name: "Ignews",
+    name: "ignew",
     version: "1",
   },
 });
